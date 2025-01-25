3d14f6048e46a58c0821e3449b359bc375b59250
shubham kopare <149860270+shubhamkopare1@users.noreply.github.com>
2025-01-25 21:14:25 +0530

    some binary tree quetion

--- Trees.java
+++ Trees.java
@@ -97,29 +97,29 @@ import java.util.*;
 
 
 public class tree{
-    static class Node{
-          int data;
-          Node left;
-          Node right;
-          Node(int data){
-            this.data = data;
-            this.left = null;
-            this.right = null;
-          }
-    }
+    // static class Node{
+    //       int data;
+    //       Node left;
+    //       Node right;
+    //       Node(int data){
+    //         this.data = data;
+    //         this.left = null;
+    //         this.right = null;
+    //       }
+    // }
 
 
     
-        public static int maxHeight(Node root){
-             if(root == null){
-                return 0;
-             }
+        // public static int maxHeight(Node root){
+        //      if(root == null){
+        //         return 0;
+        //      }
 
-             int lh = maxHeight(root.left);
-             int rh = maxHeight(root.right);
-             int max = Math.max(lh,rh)+1;
-             return max;
-        }
+        //      int lh = maxHeight(root.left);
+        //      int rh = maxHeight(root.right);
+        //      int max = Math.max(lh,rh)+1;
+        //      return max;
+        // }
 
         //   public static int count(Node root){
         //     if(root == null){
@@ -140,38 +140,104 @@ public class tree{
 
         // }
 
-    public static class Info {
-        int diam;
-        int ht;
+    // public static class Info {
+    //     int diam;
+    //     int ht;
 
-        public Info(int diam, int ht) {
-            this.diam = diam;
-            this.ht = ht;
+    //     public Info(int diam, int ht) {
+    //         this.diam = diam;
+    //         this.ht = ht;
+    //     }
+    // }
+    //        public static Info diameter(Node root){
+    //           if(root == null){
+    //             return new Info(0,0);
+    //           }
+    //             Info leftInfo = diameter(root.left);
+    //             Info rightInfo = diameter(root.right);
+
+    //             int diam = Math.max(Math.max(leftInfo.diam,rightInfo.diam),leftInfo.ht + rightInfo.ht +1);
+    //             int ht = Math.max(leftInfo.ht, rightInfo.ht)+1;
+    //             return new Info(diam,ht);
+    //         }
+
+static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
         }
     }
-           public static Info diameter(Node root){
-              if(root == null){
-                return new Info(0,0);
-              }
-                Info leftInfo = diameter(root.left);
-                Info rightInfo = diameter(root.right);
 
-                int diam = Math.max(Math.max(leftInfo.diam,rightInfo.diam),leftInfo.ht + rightInfo.ht +1);
-                int ht = Math.max(leftInfo.ht, rightInfo.ht)+1;
-                return new Info(diam,ht);
+    static class Info {
+        Node node;
+        int hd;
+
+        Info(Node node, int hd) {
+            this.node = node;
+            this.hd = hd;
+        }
+    }
+
+    public static void topView(Node root) {
+        if (root == null) return;
+
+        Queue<Info> q = new LinkedList<>();
+        HashMap<Integer, Node> map = new HashMap<>();
+        int min = 0, max = 0;
+
+        q.add(new Info(root, 0));
+
+        while (!q.isEmpty()) {
+            Info curr = q.remove();
+
+            // If horizontal distance is encountered for the first time
+            if (!map.containsKey(curr.hd)) {
+                map.put(curr.hd, curr.node);
             }
 
-    public static void main(String args[]){
+            if (curr.node.left != null) {
+                q.add(new Info(curr.node.left, curr.hd - 1));
+                min = Math.min(min, curr.hd - 1);
+            }
+
+            if (curr.node.right != null) {
+                q.add(new Info(curr.node.right, curr.hd + 1));
+                max = Math.max(max, curr.hd + 1);
+            }
+        }
+
+        // Print the top view
+        for (int i = min; i <= max; i++) {
+            System.out.print(map.get(i).data + " ");
+        }
+    }
+public static void kLevel(Node root, int level ,int k){
+    if(root == null){
+        return;
+    }
+    if(level == k){
+        System.out.print(root.data);
+        return;
+    }
+    kLevel(root.left,level+1, k);
+    kLevel(root.right,level+1, k);
+}
+    public static void main(String args[]) {
         Node root = new Node(1);
         root.left = new Node(2);
         root.right = new Node(3);
         root.left.left = new Node(4);
         root.left.right = new Node(5);
         root.right.left = new Node(6);
-        root.right.right= new Node(7);
-        // System.out.print(maxHeight(root));
-        // System.out.print(count(root));
-        // System.out.print(sumNode(root));
-        System.out.print(diameter(root).diam);
+        root.right.right = new Node(7);
+
+        System.out.print("Top View: ");
+        // topView(root);
+        kLevel(root,1,3);
     }
 }
