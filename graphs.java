cf21ba981f8eef3f3fd942da9b9709a50023cf3a
shubham kopare <149860270+shubhamkopare1@users.noreply.github.com>
2025-01-28 16:49:45 +0530

    add graphs

--- graphs.java
+++ graphs.java
@@ -10,11 +10,8 @@ class Graph{
             this.wt = w;
            }
       }
-      @SuppressWarnings("unchecked")
-    public static void main(String args[]){
-          int v =5;
-          ArrayList<Edge>[] graph = new ArrayList[v];
-          for(int i= 0; i< graph.length; i++){
+      public static void createGraph( ArrayList<Edge>[] graph){
+  for(int i= 0; i< graph.length; i++){
             graph[i] = new ArrayList<>();
           }
          graph[0].add(new Edge(0, 1, 5));
@@ -27,9 +24,62 @@ class Graph{
         graph[3].add(new Edge(3, 2, 1));
         graph[3].add(new Edge(3, 1, 3));
         graph[4].add(new Edge(4, 2, 2));
-         for(int i=0; i< graph[2].size(); i++){
-            System.out.print(graph[2].get(i).dest);
-         }
+         // for(int i=0; i< graph[2].size(); i++){
+         //    System.out.print(graph[2].get(i).dest);
+         // }
+      }
+      // public static void BFS( ArrayList<Edge>[] graph){
+      //   Queue<Integer> q = new LinkedList<>();
+      // boolean[] visited = new boolean[graph.length];
+      //   q.add(0);
+      //   while(!q.isEmpty()){
+      //      int curr=  q.remove();
+      //      if(!visited[curr]){
+      //       System.out.print(curr);
+      //       visited[curr] = true;
+      //       for(int i=0; i< graph[curr].size(); i++){
+      //           int neighbor = graph[curr].get(i).dest;
+      //             if (!visited[neighbor]) {
+      //               q.add(neighbor);
+      //           }
+      //       }
+      //      }
+
+
+      //   }
+
+      // }
+      // public static void dfs(ArrayList<Edge>[] graph , int curr , boolean vis[]){
+      //     vis[curr] = true;
+      //     for(int i= 0; i<graph(curr).size(); i++){
+      //       int e = graph(curr).get(i);
+      //       if(!vis[curr]){
+      //         dfs(graph,e.dest,vis);
+      //       }
+      //     }
+      // }
+      public static boolean hashPath(ArrayList<Edge>[] graph, int src, int dest, boolean visit[]){
+        if(src == dest){
+          return true;
+        }
+        visit[src] = true;
+        for(int i=0; i<graph[src].size(); i++){
+          Edge e = graph[src].get(i);
+          if(!visit[e.dest] && hashPath(graph, e.dest, dest, visit)){
+            return true;
+          }
+        }
+        return false;
+      }
+      @SuppressWarnings("unchecked")
+    public static void main(String args[]){
+          int v =5;
+          ArrayList<Edge>[] graph = new ArrayList[v];
+          createGraph(graph);
+          // BFS(graph);
+          System.out.print(hashPath(graph,0,5,new boolean[v]
+));
+        
     
 }
 }
